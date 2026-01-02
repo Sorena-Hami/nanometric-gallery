aa060af22ca83794e353193087f357743d9ac748
Sorena-Hami <pycel.ir@gmail.com>
2026-01-02 05:53:03 +0330

    Add backupData array with image details

---
+++ data.js
@@ -0,0 +1,12 @@
+const backupData = [
+    {
+        id: "NANO-1001", cat: "ترمیم عکس", img: "https://images.unsplash.com/photo-1548695602-5e49c7173b4d?auto=format&fit=crop&w=500&q=80",
+        title: "ترمیم عکس قدیمی", price: "200000", off: "150000",
+        views: 1200, likes: 350, score: 4.8, crowns: '👑👑👑👑'
+    },
+    {
+        id: "NANO-1002", cat: "طراحی چهره", img: "https://images.unsplash.com/photo-1579783902614-a3fb39279c0f?auto=format&fit=crop&w=500&q=80",
+        title: "نقاشی دیجیتال", price: "0", off: "0",
+        views: 800, likes: 200, score: 4.2, crowns: '👑👑👑'
+    }
+];
